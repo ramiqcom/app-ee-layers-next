ac07a312c4d528226a9e9971958f99ad689f75b3
Ramadhan <ramiqcom@gmail.com>
2024-02-26 09:32:23 +0100

    update welcome

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
-      Welcome to ee-layers-next
+      Welcome to ee-layers-next. Use /layers endpoint to POST layer request.
     </main>
   );
 }
